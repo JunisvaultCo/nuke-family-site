50baac500e20febd43e3d1a789042e503a2fd632
JunisvaultCo <48123844+JunisvaultCo@users.noreply.github.com>
2019-03-01 21:45:22 +0200

    Added config.js for modularity

---
+++ config.js
@@ -0,0 +1,16 @@
+{
+  "factions":{
+    "12863":{
+      "update" : true
+    },
+    "13851":{
+      "update" : false
+    },
+    "8085":{
+      "update" : true
+    },
+    "8954":{
+      "update" : true
+    }
+  }
+}
